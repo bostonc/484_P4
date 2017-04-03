37212d98bda015d1cdd51e2c6e993c1279cfadb1
mrmintz <mrmintz@umich.edu>
2017-04-02 21:14:49 -0400

    public functions

--- StudentComponent/LogMgr.cpp
+++ StudentComponent/LogMgr.cpp
@@ -1,40 +1,72 @@
-//LogMgr Public functions to implement, there are also private functions 
+#include "LogMgr.h"
+
+//LogMgr Private functions to implement
+
+
+
+
+//LogMgr Public functions to implement
 
 /*
    * Abort the specified transaction.
    * Hint: you can use your undo function
    */
-  void abort(int txid);
+  void LogMgr::abort(int txid) {
+    //TODO
+  }
 
   /*
    * Write the begin checkpoint and end checkpoint
    */
-  void checkpoint();
+  void LogMgr::checkpoint() {
+    //TODO
+  }
 
   /*
    * Commit the specified transaction.
    */
-  void commit(int txid);
+  void LogMgr::commit(int txid) {
+      //TODO
+  }
 
   /*
    * A function that StorageEngine will call when it's about to 
    * write a page to disk. 
    * Remember, you need to implement write-ahead logging
    */
-  void pageFlushed(int page_id);
+  void LogMgr::pageFlushed(int page_id) {
+      //TODO
+  }
 
   /*
    * Recover from a crash, given the log from the disk.
    */
-  void recover(string log);
+  void LogMgr::recover(string log) {
+    //TODO
+  }
 
   /*
    * Called by StorageEngine whenever an update is called
    * LogMgr should update tables if required and return the LSN of the action performed
    */
-  int write(int txid, int page_id, int offset, string input, string oldtext);
+  int LogMgr::write(int txid, int page_id, int offset, string input, string oldtext) {
+      //TODO
+  }
 
   /*
    * Sets this.se to engine. 
    */
-  void setStorageEngine(StorageEngine* engine);
+  void LogMgr::setStorageEngine(StorageEngine* engine) {
+      //TODO
+  }
+
+
+
+
+
+
+
+
+
+
+
